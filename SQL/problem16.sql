1aa28396842718da63d01995ef4d4959f647f777
shaktimnepal <shaktimnepal1@gmail.com>
2024-09-17 16:50:22 -0700

    Update problem16.sql

--- SQL/problem16.sql
+++ SQL/problem16.sql
@@ -75,6 +75,8 @@ Customer with id = 30 visited the mall once and did not make any transactions.
 Customer with id = 54 visited the mall three times. During 2 visits they did not make any transactions, and during one visit they made 3 transactions.
 Customer with id = 96 visited the mall once and did not make any transactions.
 As we can see, users with IDs 30 and 96 visited the mall one time without making any transactions. Also, user 54 visited the mall twice and did not make any transactions.
+
+
 SOLUTION:
 SELECT v.customer_id, COUNT(*) AS count_no_trans
 FROM visits v
